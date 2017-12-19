0eb5b11c0f687cc97a9513d51a338dd19dc2ba62
javi1966 <JEG1966@telefonica.net>
2017-12-19 19:13:45 +0100

    primer commit

---
+++ www/js/index.js
@@ -0,0 +1,102 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+var app = {
+    // Application Constructor
+    hora: "",
+    minu: "",
+    seg: "",
+    punto:false,
+    initialize: function() {
+        this.bindEvents();
+        setInterval('app.reloj()', 5999);
+        console.log("initialize: ");
+    },
+
+    // deviceready Event Handler
+    //
+    // Bind any cordova events here. Common events are:
+    // 'pause', 'resume', etc.
+    bindEvents: function () {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+        //$(document).on('pageshow', '#main', this.onPageShow);
+        
+        console.log("bindEvents:");
+    },
+    
+    onDeviceReady: function () {
+        app.receivedEvent('deviceready');
+        //$(document).bind("resume", app.onResumedApp);
+        app.reloj();
+        console.log("onDeviceReady");
+    },
+    reloj: function () {
+        var hora = new Date();
+        var strHora="";
+        var temperatura="";
+        var humedad="";
+        var presion="";
+         
+        app.hora = hora.getHours();
+        app.minu = hora.getMinutes();
+        app.seg  = hora.getSeconds();
+        
+        app.minu = app.minu > 9 ? app.minu : '0' + app.minu;
+        strHora =  app.hora + ':' + app.minu;
+                                
+        
+        $("#idHora span:first").text(app.hora);
+        $("#idHora span:last").text(app.minu);
+        
+        $("#idFecha").text(hora.toLocaleDateString("es"));
+                
+        
+        $.getJSON('http://api.thingspeak.com/channels/172131/feeds/last.json?api_key=SQ0AWSJRWFJ8Z7O1')
+                //, function (vj) {
+                .done(function (data) {
+                    
+                    temperatura=data.field1;
+                    humedad=data.field2;
+                    presion=data.field3;
+                    $("#idTemp").text(temperatura.slice(0,2));
+                    $("#idHumedad").text(humedad.slice(0,2));
+                    $("#idPresion").text(presion.slice(0,3));
+                    
+                    console.log("Temperatura: " + data.field1);
+                    console.log("Humedad: " + data.field2);
+                    console.log("Presion: " + data.field3);
+                })
+
+                .error(function () {
+                     
+                      console.log("Error comunicacion");
+                });  
+        
+        
+        
+        
+        console.log("reloj: "+strHora);
+    },
+
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        
+        console.log('Received Event: ' + id);
+    }
+};
+
